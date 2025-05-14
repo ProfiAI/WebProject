ad8ae8da70edc53680a175aeb8959ada6ed68d93
MQD-SQU <s140335@student.squ.edu.om>
2025-05-14 15:31:16 +0400

    Add files via upload

---
+++ qustionnairephp.php
@@ -0,0 +1,45 @@
+<html>
+    <body>
+
+            
+        <?php
+
+            $userName = "";
+            $email = "";
+            $Text = "";
+            $page = "";
+            $subscribe ="";
+
+
+
+            if ($_SERVER["REQUEST_METHOD"] == "GET"){
+
+                $userName = test_input($_GET["userName"]);
+                $email = test_input($_GET["email"]);
+                $text = test_input($_GET["Text"]);
+                $page = test_input($_GET["page"]);
+                $subscribe = test_input($_GET["subscribe"]);
+                
+
+            }
+
+            function test_input($data) {
+                $data = trim($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+
+
+            echo "  <table border=\"1\" cellpadding=\"10\">
+            <a href = \"index.html\"> go to Home page</a>
+                        <tr>    <th>Full Name</th>              <td>".$userName."</td>   </tr>
+                        <tr>    <th>email</th>           <td>".$email."</td>   </tr>
+                        <tr>    <th>Questionnaire</th>   <td>".$text."</td>   </tr>
+                        <tr>    <th>page</th>                <td>".$page."</td>   </tr>
+                        <tr>    <th>subscribe</th>      <td>".$subscribe."</td>    </tr>;
+                        
+                    </table>";
+
+        ?>
+    </body>
+</html>
