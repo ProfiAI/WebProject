2edb7f793b13997f8752b7bba3a50518ab1940e4
MQD-SQU <s140335@student.squ.edu.om>
2025-05-14 17:38:17 +0400

    Update contactUsphp.php

--- contactUsphp.php
+++ contactUsphp.php
@@ -1,5 +1,13 @@
 <html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
     <body>
+        <div class="container" class="p-5 m-5">
+
+        <br><br><br>
+
 
             
         <?php
@@ -17,8 +25,6 @@
                 $email = test_input($_GET["email"]);
                 $subject = test_input($_GET["subject"]);
                 $message = test_input($_GET["message"]);
-                
-
             }
 
             function test_input($data) {
@@ -28,15 +34,27 @@
             }
 
 
-            echo "  <table border=\"1\" cellpadding=\"10\">
-            <a href = \"index.html\"> go to Home page</a>
-                        <tr>    <th>Full Name</th>              <td>".$fullName."</td>   </tr>
-                        <tr>    <th>email</th>           <td>".$email."</td>   </tr>
-                        <tr>    <th>subject</th>   <td>".$subject."</td>   </tr>
-                        <tr>    <th>message</th>                <td>".$message."</td>   </tr>
-                        
-                    </table>";
+            function displayTable($data){
+                $display = "";
+                $display .= "  <table class='table table-hover table-striped' border=\"1\" cellpadding=\"10\">";
+
+                foreach ($data as $key => $value) {
+                    $display .= "<tr>    <th>$key</th>              <td>".$value."</td>   </tr>";
+                }
+                $display .= "</table>";
+
+                return $display;
+            }
+
+            echo displayTable($_GET);
 
         ?>
+
+
+        <p class = "p-3">
+            <a class="btn btn-primary" href = "index.html"> go to Home page</a>
+        </p>
+    </div>
+
     </body>
 </html>
