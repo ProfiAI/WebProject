ca8d4f109a0e688c6c05d1e9b8359fd1818a3fcc
MQD-SQU <s140335@student.squ.edu.om>
2025-05-14 17:37:37 +0400

    Update qustionnairephp.php

--- qustionnairephp.php
+++ qustionnairephp.php
@@ -1,49 +1,70 @@
 <html>
+
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
     <body>
 
-            
-        <?php
+    <div class="container" class="p-5 m-5">
 
-            $userName = "";
-            $email = "";
-            $text = "";
-            $page = "";
-            $subscribe ="";
+        
 
+        <br><br><br>
 
-
-            if ($_SERVER["REQUEST_METHOD"] == "GET"){
-
-                $userName = test_input($_GET["userName"]);
-                $email = test_input($_GET["email"]);
-                $text = test_input($_GET["Text"]);
-                $page = test_input($_GET["page"]);
-
-                $subscribe = isset($_GET["subscribe"]);
 
                 
+            <?php
 
-            }
-
-            function test_input($data) {
-                $data = trim($data);
-                $data = htmlspecialchars($data);
-                return $data;
-            }
-
-
-            echo "  <table border=\"1\" cellpadding=\"10\">
-                        <tr>    <th>Full Name</th>              <td>".$userName."</td>   </tr>
-                        <tr>    <th>email</th>           <td>".$email."</td>   </tr>
-                        <tr>    <th>Questionnaire</th>   <td>".$text."</td>   </tr>
-                        <tr>    <th>page</th>                <td>".$page."</td>   </tr>
-                        <tr>    <th>subscribe</th>      <td>".$subscribe."</td>    </tr>
-                        
-                    </table>";
+                $userName = "";
+                $email = "";
+                $text = "";
+                $page = "";
+                $subscribe ="";
 
 
-            print("<a href = \"index.html\"> go to Home page</a>");
 
-        ?>
+                if ($_SERVER["REQUEST_METHOD"] == "GET"){
+
+                    $userName = test_input($_GET["userName"]);
+                    $email = test_input($_GET["email"]);
+                    $text = test_input($_GET["Text"]);
+                    $page = test_input($_GET["page"]);
+
+                    $subscribe = isset($_GET["subscribe"]);
+
+                    
+
+                }
+
+                function test_input($data) {
+                    $data = trim($data);
+                    $data = htmlspecialchars($data);
+                    return $data;
+                }
+
+
+                function displayTable($data){
+                    $display = "";
+                    $display .= "  <table class='table table-hover table-striped' border=\"1\" cellpadding=\"10\">";
+
+                    foreach ($data as $key => $value) {
+                        $display .= "<tr>    <th>$key</th>              <td>".$value."</td>   </tr>";
+                    }
+                    $display .= "</table>";
+
+                    return $display;
+                }
+
+                echo displayTable($_GET);
+
+            ?>
+
+
+        <p class = "p-3">
+            <a class="btn btn-primary" href = "index.html"> go to Home page</a>
+        </p>
+
+    </div>
     </body>
 </html>
